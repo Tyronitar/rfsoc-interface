b89d82fbb8d40280e48c01d4198986cb9926b4d3
Nia McNichols <niamcnichols@gmail.com>
2025-02-27 13:37:35 -0800

    Update Cody's Code

--- rfsocinterface/analysis/compute_noise_psd.py
+++ rfsocinterface/analysis/compute_noise_psd.py
@@ -8,6 +8,7 @@ from matplotlib.figure import Figure
 from scipy import signal, ndimage, fftpack
 from matplotlib.backends.backend_pdf import PdfPages
 from scipy.optimize import curve_fit
+import scipy
 import pdb
 import h5py
 import argparse
@@ -45,6 +46,8 @@ def compute_noise_psd(
     lp_filter_template2: float=25.,
     flag_outliers: bool=True,
     outlier_sigma: float=4,
+    cody_title: str='',
+    cody_file: str='',
 ) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]:
     """Compute noise PSD.
 
@@ -127,12 +130,41 @@ def compute_noise_psd(
         # n_samples_per_block = (2 ** np.floor(np.log2(n_samples))).astype(int)
         n_samples_per_block = n_samples
     
+    if cody_title:
+        return cody_psd(new_input_data[:, np.where(chanmask == 1)[0], :], fs, n_samples_per_block, cody_file, cody_title)
+    else:
+        return psd(
+            new_input_data,
+            fs,
+            chanmask,
+            n_samples_per_block,
+            n_blocks,
+            hp_filter_template=hp_filter_template,
+            lp_filter_template=lp_filter_template,
+            lp_filter_template2=lp_filter_template2,
+    )
+
+def psd(
+        new_input_data: npt.NDArray,
+        fs: float,
+        chanmask: npt.NDArray,
+        n_samples_per_block: int,
+        n_blocks: int,
+        hp_filter_template: float=0.05,
+        lp_filter_template: float=115,
+        lp_filter_template2: float=25
+):
+    n_chan = np.size(chanmask)
+    first_dimension = new_input_data.shape[0]
+
     # Window for the PSD
     wind = signal.get_window('hamming', n_samples_per_block)
 
     psd_all = np.zeros((first_dimension, n_chan, int(n_samples_per_block / 2 + 1)))
     psd_all_clean = np.zeros((first_dimension, n_chan, int(n_samples_per_block / 2 + 1)))
+    # freq, _ = signal.welch(np.ones(n_samples_per_block), fs)
     freq, _ = signal.periodogram(np.ones(n_samples_per_block), fs)
+
 
     #figure out an average template to try to remove thermal fluctuations
     data_all = new_input_data[:, np.where(chanmask == 1)[0],:]
@@ -174,7 +206,8 @@ def compute_noise_psd(
                     idx,
                     i_block * n_samples_per_block : (i_block + 1) * n_samples_per_block
                 ]
-                _, this_psd = signal.periodogram(this_data, fs, window=wind)
+                # _, this_psd = signal.periodogram(this_data, fs, window=wind)
+                _, this_psd = signal.welch(this_data, fs, window=wind)
                 psd += this_psd / float(n_blocks)
 
                 #correlate with average template, subtract polynomial, then computed power spectrum
@@ -203,7 +236,8 @@ def compute_noise_psd(
                 clean_data = this_data_filt - template_corr * this_template_filt
                 pfit = np.polyfit(dummy_time, clean_data, 2)
                 clean_data = clean_data - np.polyval(pfit, dummy_time)
-                _, this_psd = signal.periodogram(clean_data, fs, window=wind)
+                # _, this_psd = signal.periodogram(clean_data, fs, window=wind)
+                _, this_psd = signal.welch(clean_data, fs, window=wind)
                 psd_clean = psd_clean + this_psd / float(n_blocks)
 
             psd_all[i_complex, i_chan, :] = psd
@@ -215,6 +249,147 @@ def compute_noise_psd(
     # plt.show()
 
     return chanmask, freq, psd_all, psd_all_clean
+
+def cody_psd(data, fs, npoints, file_name, title):
+    I = data[0]
+    Q = data[1]
+    templates = cody_template(I, Q)
+    I_clean, Q_clean = cody_clean(I, Q, *templates)
+
+    # fig1 = cody_plot(I, Q, 8192, fs, file_name)
+    fig = cody_plot(I_clean, Q_clean, npoints, fs, title)
+    with PdfPages(f'plots/cody_{file_name}.pdf') as pdf:
+        pdf.savefig(fig)
+
+
+def cody_template(I, Q):
+    # subtract the mean from each detector
+    Imeansub = np.zeros_like(I)
+    Qmeansub = np.zeros_like(Q)
+    for i in range(len(I[:,0])):
+        Imeansub[i,:] = I[i,:] - np.mean(I[i,:])
+        Qmeansub[i,:] = Q[i,:] - np.mean(Q[i,:])
+    
+    # select only the middle few detectors
+    deproj_I = Imeansub[8:1008 ,:]
+    deproj_Q = Qmeansub[8:1008,:]
+
+    # create a separate correlation matrix for I and Q
+    correlation_matrix_I = np.matmul(deproj_I,np.conj(np.transpose(deproj_I)))
+    correlation_matrix_Q = np.matmul(deproj_Q,np.conj(np.transpose(deproj_Q)))
+    # calculate the eigenmodes of each correlation matrix
+    wI,vI = np.linalg.eig(correlation_matrix_I)
+    wQ,vQ = np.linalg.eig(correlation_matrix_Q)
+    # create templates based on the largest eigenmode of each
+    templateI0 = np.matmul(vI[:,0],deproj_I)    
+    templateQ0 = np.matmul(vQ[:,0],deproj_Q)
+
+    # subtract the mean again to be sure
+    template_real0 = np.real(templateI0)-np.mean(np.real(templateI0))
+    template_imag0 = np.real(templateQ0)-np.mean(np.real(templateQ0))
+
+    # create templates based on the second largest eigenmode of each
+    templateI1 = np.matmul(vI[:,1],deproj_I)    
+    templateQ1 = np.matmul(vQ[:,1],deproj_Q)
+
+    # subtract the mean again to be sure
+    template_real1 = np.real(templateI1)-np.mean(np.real(templateI1))
+    template_imag1 = np.real(templateQ1)-np.mean(np.real(templateQ1))
+   
+    #plt.figure()
+    #plt.subplot(2,1,1)
+    #plt.semilogy(wI,"x")
+    #plt.grid("on")
+    #plt.subplot(2,1,2)
+    #plt.semilogy(wQ,"x")
+    #plt.grid("on")
+    
+    return template_real0,template_imag0,template_real1,template_imag1
+
+def cody_clean(I,Q,template_I0,template_Q0,template_I1,template_Q1):
+    Iclean = np.zeros_like(I)
+    Qclean = np.zeros_like(Q)
+    
+    for idet in range(len(I[:,0])):
+        samp_chan_I = I[idet,:]
+        samp_chan_Q = Q[idet,:]
+    
+        corr0 = np.matmul(samp_chan_I,np.transpose(template_I0))/np.matmul(template_I0,np.transpose(template_I0))
+        deprojected_samp_detector_I = samp_chan_I-corr0*template_I0
+        corr1 = np.matmul(deprojected_samp_detector_I,np.transpose(template_I1))/np.matmul(template_I1,np.transpose(template_I1))
+        deprojected_samp_detector_I = deprojected_samp_detector_I-corr1*template_I1
+        
+        corr0 = np.matmul(samp_chan_Q,np.transpose(template_Q0))/np.matmul(template_Q0,np.transpose(template_Q0))
+        deprojected_samp_detector_Q = samp_chan_Q-corr0*template_Q0
+        corr1 = np.matmul(deprojected_samp_detector_Q,np.transpose(template_Q1))/np.matmul(template_Q1,np.transpose(template_Q1))
+        deprojected_samp_detector_Q = deprojected_samp_detector_Q-corr1*template_Q1
+    
+        Iclean[idet,:] = deprojected_samp_detector_I
+        Qclean[idet,:] = deprojected_samp_detector_Q
+    return Iclean,Qclean
+
+def cody_plot(I, Q, npoints, fs, title):
+    # initialize variables
+    nfs = int(np.log2(npoints))
+    f_interp = np.zeros(nfs)
+    Spp_i_interp = np.zeros((1024,nfs))
+    Spp_q_interp = np.zeros((1024,nfs))
+    Spp_interp = np.zeros((1024,nfs))
+
+    # find the power in each detector
+    rms_per_det = np.std(I,axis=1) + np.std(Q,axis=1)
+    # find the detector with median power
+    idet_median = np.argsort(rms_per_det)[int(len(rms_per_det)/2)]
+    fig = plt.figure(figsize=(12,8))
+    #for idet in range(4,14):
+    for idet in range(len(I[:,0])):
+        # form the complex signal of this detector
+        Z = I[idet,:] + 1j*Q[idet,:]
+
+        # calculate the DC value for this detector
+        norm = np.mean(np.abs(Z))
+
+        # take the Welch periodogram (viewer-friendly FFT)
+        # of the real and imaginary parts of the signal
+        # f,Spp_i=scipy.signal.welch(np.real(Z)/norm,fs=512e6/2**(10+10),nperseg=npoints)
+        # f,Spp_q=scipy.signal.welch(np.imag(Z)/norm,fs=512e6/2**(10+10),nperseg=npoints)
+        f,Spp_i=scipy.signal.welch(np.real(Z)/norm,fs=fs,nperseg=npoints)
+        f,Spp_q=scipy.signal.welch(np.imag(Z)/norm,fs=fs,nperseg=npoints)
+
+        # smooth the curve further for plotting
+        for j in range(0,nfs-1):
+            f_interp[j] = np.mean(f[2**j:2**(j+1)])
+            Spp_i_interp[idet,j] = np.mean(Spp_i[2**j:2**(j+1)])
+            Spp_q_interp[idet,j] = np.mean(Spp_q[2**j:2**(j+1)])
+
+        # combine I and Q spectra for plotting
+        Spp_interp[idet,:] = (Spp_i_interp[idet,:] + Spp_q_interp[idet,:])/2
+
+        # plot this detector
+        plt.semilogx(f_interp[:],10*np.log10(Spp_i_interp[idet,:])[:],"-",color="cyan",alpha=0.2)#,label="All Detectors")
+        plt.semilogx(f_interp[:],10*np.log10(Spp_q_interp[idet,:])[:],"-",color="cyan",alpha=0.2)#,label="All Detectors")
+        # plot non smoothed data
+        # plt.semilogx(f[:],10*np.log10(Spp_i[:])[:],"-",color="blue",alpha=0.04)#,label="All Detectors")
+        # plt.semilogx(f[:],10*np.log10(Spp_q[:])[:],"-",color="orange",alpha=0.04)#,label="All Detectors")
+        # if we are at the detector with median power
+        if idet == idet_median:
+            # plot this again, but with a solid black line to highlight it
+            plt.semilogx(f_interp[:],10*np.log10(Spp_interp[idet,:])[:],"-",color="black",alpha=1.00,label="Median Detectors", zorder=99999)
+    
+    # fname = 'OUT/'+file.split('/')[1].split('.')[0]
+    #fname = filename
+    
+    plt.title(title)# 20-900 Channel 2")
+    plt.ylabel(r"$S_{\phi \phi}$ [dBc/Hz]", fontsize=16); 
+    plt.xlabel("Hz", fontsize=16)
+    plt.ylim(-140,-40)
+    plt.yticks(np.linspace(-40, -140, 21))
+    #plt.yticks(np.linspace(-110, -50, abs(110-50)-1))
+    plt.grid()
+    # plt.savefig(fname)
+    # plt.show()
+    # print(fname)
+    return fig
 
 @ensure_path(4)
 def plot_psd(
@@ -274,11 +449,20 @@ def plot_psd(
         percentiles=(min_percentile, max_percentile),
         title=titles[1],
     )
+    fig2 = create_plot(
+        freq,
+        (plot_data_min[0] + plot_data_min[1]) / 2,
+        (plot_data_med[0] + plot_data_med[1]) / 2,
+        (plot_data_max[0] + plot_data_max[1]) / 2,
+        percentiles=(min_percentile, max_percentile),
+        title= title + ' - Combined',
+    )
     with PdfPages(filename) as pdf:
         pdf.savefig(fig0)
         pdf.savefig(fig1)
+        pdf.savefig(fig2)
 
-    return fig0, fig1
+    return fig0, fig1, fig2
 
 def create_plot(
         xdata: npt.ArrayLike,
@@ -405,15 +589,30 @@ if __name__ == '__main__':
     ]
     for name, title in pairs:
         input_data, timestamp, chanmask = load_data(f'data/{name}.hdf5')
+        rotate = True
         rotated_data = rotate_to_amplitude_and_phase(input_data)
+        save_name = f'welch_{name}'
+        # Do Cody's stuff with the I/Q data
+        # compute_noise_psd(
+        #     input_data,
+        #     timestamp,
+        #     chanmask=None,
+        #     ds_factor=3,
+        #     flag_outliers=True,
+        #     nominal_block_length=10,
+        #     outlier_sigma=2,
+        #     cody_file=save_name,
+        #     cody_title=title,
+        # )
         chanmask, freq, psd_all, psd_all_clean = compute_noise_psd(
             rotated_data,
             timestamp,
             chanmask=None,
             ds_factor=3,
-            flag_outliers=False,
+            flag_outliers=True,
             nominal_block_length=10,
+            outlier_sigma=2,
         )
-        fig = plot_psd(chanmask, freq, psd_all, psd_all_clean, f'plots/{name}.pdf', title=title)
+        figs = plot_psd(chanmask, freq, psd_all, psd_all_clean, f'plots/{save_name}.pdf', basis='pa', title=title)
     # plt.show()
 
