Fix Units in LO Sweep
* Started updating some of the plotting code

rfsocinterface/core/losweep.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
 from matplotlib.figure import Figure
+from matplotlib.ticker import FormatStrFormatter, EngFormatter, FuncFormatter
 import h5py
 
 from onr_fit_lo_sweeps import simple_derivative_fits
@@ -28,6 +29,19 @@ from kidpy3.data_handler import Rfchan
 
 
 BAD_RFSOC_TONE_START_INDEX = 8  # First 8 ones are bad...
+
+
+def resonator_plot_formatter(x: float, pos: int) -> str:
+    """Format the x-axis labels for the resonator plot, converting to MHz.
+
+    Arguments:
+        x (float): The x value to format.
+        pos (int): The position of the tick.
+
+    Returns:
+        str: The formatted string for the x-axis label.
+    """
+    return f'{x * 1e-6:.3f}'
 
 
 class ResonatorData:
@@ -69,22 +83,24 @@ class ResonatorData:
             return_fig = True
             ax.set_title(f'Transmission Magnitude near Resonator #{self.idx}')
             ax.set_xlabel('Frequency (MHz)')
-            ax.ticklabel_format(useOffset=False, style='plain')
+            # ax.ticklabel_format(useOffset=False, style='plain')
             ax.set_ylabel(r'$|S_{21}|$')
+            # ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+            ax.xaxis.set_major_formatter(FuncFormatter(resonator_plot_formatter))
         # Otherwise, just plot inside the existing axes
         else:
             ax.set_facecolor('white')
             ax.set_yticks([])
             ax.set_xticks([])
 
-        ax.plot(self.freq * 1e-6, self.s21)
-        ax.axvline(x=self.fit_f0 * 1e-6, color='r', animated=animated)
+        ax.plot(self.freq, self.s21)
+        ax.axvline(x=self.fit_f0, color='r', animated=animated)
 
         # Scale the span of the plot based on the frequency ratio
-        new_span = self.span * 1e-6 * self.freq_ratio
+        new_span = self.span * self.freq_ratio
         ax.set_xlim(
-            np.mean(self.freq * 1e-6 - new_span / 2.0),
-            np.mean(self.freq * 1e-6 + new_span / 2.0),
+            np.mean(self.freq - new_span / 2.0),
+            np.mean(self.freq + new_span / 2.0),
         )
 
         # Add a label showing the resonator number
@@ -108,7 +124,7 @@ class ResonatorData:
                 [
                     f'{self.idx:d}'
                     + ', dS21='
-                    + f'{np.max(self.s21) - np.min(self.s21):4.1f}'
+                    + f'{np.ptp(self.s21):4.1f}'
                 ],
                 fontsize=6,
                 loc=3,
@@ -126,9 +142,14 @@ class ResonatorData:
         return None
 
     @property
-    def tone(self) -> float:
-        """float: The original tone for this resonator from the tone list, in Hz."""
+    def baseband_tone(self) -> float:
+        """float: The tone for this resonator relative to the f center, in Hz."""
         return self.data.tone_list[self.idx]
+
+    @property
+    def tone(self) -> float:
+        """float: The absolute tone for this resonator, in Hz."""
+        return self.data.tone_list[self.idx] + self.data.f_center
 
     @property
     def freq(self) -> npt.NDArray:
@@ -142,8 +163,8 @@ class ResonatorData:
 
     @property
     def difference(self) -> float:
-        """float: The difference in the fitted value and the original tone, in KHz."""
-        return (self.data.fit_f0[self.idx] - self.data.tone_list[self.idx]) * 1e-3
+        """float: The difference in the fitted value and the original tone, in Hz."""
+        return np.abs(self.data.fit_f0[self.idx] - self.data.tone_list[self.idx])
 
     @property
     def is_onres(self) -> bool:
@@ -153,21 +174,21 @@ class ResonatorData:
     @property
     def freq_ratio(self) -> float:
         """float: The ratio of the original tone and the maximum tone in the sweep."""
-        return self.tone / self.data.tone_list.max()
+        return self.tone / (self.data.tone_list.max() + self.data.f_center)
 
     @property
     def fit_f0(self) -> float:
-        """float: The fitted value for the resonance frequency."""
+        """float: The fitted value for the resonance frequency, in Hz."""
         return self.data.fit_f0[self.idx]
 
     @fit_f0.setter
     def fit_f0(self, val: float):
         self.data.fit_f0[self.idx] = val
-        self.flagged = np.abs(self.difference) > self.data.diff_to_flag[self.idx]
+        self.flagged = self.difference > self.data.diff_to_flag[self.idx]
 
     @property
     def fit_qi(self) -> float:
-        """float: The qi factor for the fitted resonance."""
+        """float: The qi factor for the fitted resonance, in Hz."""
         return self.data.fit_qi[self.idx]
 
     @fit_qi.setter
@@ -176,7 +197,7 @@ class ResonatorData:
 
     @property
     def fit_qc(self) -> float:
-        """float: The qc factor for the fitted resonance."""
+        """float: The qc factor for the fitted resonance, in Hz."""
         return self.data.fit_qc[self.idx]
 
     @fit_qc.setter
@@ -185,13 +206,13 @@ class ResonatorData:
 
     @property
     def span(self) -> float:
-        """float: The span of the frequency window for the resonator."""
+        """float: The span of the frequency window for the resonator, in Hz."""
         return np.ptp(self.freq)
 
     def fit(self, df: float, start: float = None) -> tuple[float, float, float]:
         """Perform a fit to find the resonance frequency."""
         if start is None:
-            start = self.tone
+            start = self.baseband_tone
         fit_f0 = simple_derivative_fits(df, self.freq, start, self.s21)
         fit_qi = 0.0
         fit_qc = 0.0
@@ -207,26 +228,27 @@ class LoSweepData:
 
     Attributes:
         data (npt.NDArray): The data from the LO sweep.
-        tone_list (npt.NDArray): The tone for each resonator.
-        freq (npt.NDArray): The full frequency sprectrum of the sweep.
-        s21 (npt.NDArray): The vlaue of S_{21} at all frequencies in `freq`.
+        tone_list (npt.NDArray): The tone for each resonator, in Hz.
+        freq (npt.NDArray): The full frequency sprectrum of the sweep, in Hz.
+        s21 (npt.NDArray): The value of S_{21} at all frequencies in `freq`.
         chanmask (npt.NDarray): A mask to determine which frequencies are on-resonance.
         resonator_data (list[ResonatorData]): List of the data for each resonator.
-        fit_f0 (npt.NDArray): The fitted resonance frequencies for each resonator.
+        fit_f0 (npt.NDArray): The fitted resonance frequencies for each resonator, in Hz.
         fit_qi (npt.NDArray): The qi factor of the fitted resonance frequency for each
-            resonator.
+            resonator, in Hz.
         fit_qc (npt.NDArray): The qc factor of the fitted resonance frequency for each
-            resonator.
+            resonator, in Hz.
         diff_to_flag (npt.NDArray): The mimimum difference in tone and fitted frequency
-            to flag for further inspection.
+            to flag for further inspection, in Hz.
     """
 
     def __init__(
-        self, tone_list: npt.NDArray, sweep_data: tuple[npt.NDArray, npt.NDArray], chanmask: npt.NDArray, 
+        self, tone_list: npt.NDArray, f_center: float, sweep_data: npt.NDArray, chanmask: npt.NDArray, 
     ) -> None:
         """Initialize a LoSweepData object."""
         self.data = sweep_data
-        self.tone_list = tone_list
+        self.tone_list = tone_list  # Baseband frequencies in Hz
+        self.f_center = f_center  # Center frequency of the sweep in Hz
         self.freq = np.real(self.data[0, :, :])
         self.s21 = np.real(10.0 * np.log10(np.abs(self.data[1, :, :])))
         self.chanmask = chanmask
@@ -244,7 +266,7 @@ class LoSweepData:
         Arguments:
             val (float): The minimumum difference to flag in KHz. (defaults to 3.0)
         """
-        self.diff_to_flag = (val / 200.0) * self.tone_list * 1e-6
+        self.diff_to_flag = (val * 1e3 / 200.0) * self.tone_list
     
     @classmethod
     @ensure_path(1, 2, 3)
@@ -253,7 +275,7 @@ class LoSweepData:
         tone_list = get_tone_list(tone_file, lo_freq=lo_freq)
         data = np.load(sweep_file)
         chanmask = get_chanmask(chanmask_file)
-        return cls(tone_list, data, chanmask)
+        return cls(tone_list, lo_freq, data, chanmask)
 
     @classmethod
     @ensure_path(1)
@@ -275,8 +297,8 @@ class LoSweepData:
 
     @property
     def difference(self) -> npt.NDArray:
-        """The difference of the fitted frequencies and the provided tones."""
-        return (self.fit_f0 - self.tone_list) * 1e-3
+        """The difference of the fitted frequencies and the provided tones, in Hz."""
+        return (self.fit_f0 - self.tone_list)
 
     @property
     def nchan(self) -> int:
@@ -295,7 +317,7 @@ class LoSweepData:
 
     @property
     def df(self) -> float:
-        """The difference between two frequency data points."""
+        """The difference between two frequency data points, in Hz."""
         return self.freq[0, 1] - self.freq[0, 0]
 
     @property
@@ -343,9 +365,9 @@ class LoSweepData:
                         'tone index =',
                         f'{i:4d}',
                         '|| new tone =',
-                        f'{self.fit_f0[i] * 1.0e-6:9.5f}',
+                        f'{self.fit_f0[i] * 1.0e-6:9.5f} MHz',
                         '|| old tone =',
-                        f'{self.tone_list[i] * 1.0e-6:9.5f}',
+                        f'{self.tone_list[i] * 1.0e-6:9.5f} MHz',
                         '|| difference (kHz) =',
                         f'{diff:+5.3f}',
                     )
@@ -429,12 +451,12 @@ def get_tone_list(filename: str, lo_freq: float = 400) -> npt.NDArray:
 class LoSweep:
     """Class for performing an LO Sweep"""
 
-    def __init__(self, valon: Valon5009, chan: Rfchan, freqs: npt.NDArray, f_center: float=400.0):
+    def __init__(self, valon: Valon5009, chan: Rfchan, freqs: npt.NDArray, f_center: float=400e6):
         """Initialize an LoSweep"""
         self.valon = valon
         self.chan = chan
-        self.freqs = freqs
-        self.f_center = f_center
+        self.freqs = freqs  # Hz
+        self.f_center = f_center  # Hz
         self._processed = False
 
     def _get_data(self, N_steps=500, freq_step=0.0, pd: QProgressDialog | None=None):
@@ -458,7 +480,7 @@ class LoSweep:
         Credit: Dr. Adrian Sinclair (adriankaisinclair@gmail.com)
         """
         log = logging.getLogger()
-        tone_diff = np.diff(self.freqs)[0] / 1e6  # MHz
+        tone_diff = np.diff(self.freqs)[0] * 1e-6  # MHz
         log.info(f"tone diff={tone_diff}")
         if freq_step > 0:
             flo_step = freq_step
@@ -469,7 +491,7 @@ class LoSweep:
         flo_start = self.f_center - flo_step * N_steps / 2.0  # 256
         flo_stop = self.f_center + flo_step * N_steps / 2.0  # 256
 
-        flos = np.arange(flo_start, flo_stop, flo_step) #+1e-6
+        flos = np.arange(flo_start, flo_stop, flo_step) # MHz
         if pd is not None:
             pd.setMaximum(len(flos))
             pd.setLabelText('Performing LO Sweep...')
@@ -524,7 +546,7 @@ class LoSweep:
         f = np.zeros([np.size(self.freqs), np.size(flos)])
         log.info(f"shape of f = {f.shape}")
         for itone, ftone in enumerate(self.freqs):
-            f[itone, :] = flos * 1.0e6 + ftone
+            f[itone, :] = flos * 1.0e6 + ftone  # Convert back to Hz before adding
         #    f = np.array([flos * 1e6 + ftone for ftone in freqs]).flatten()
         sweep_Z_f = sweep_Z.T
         #    sweep_Z_f = sweep_Z.T.flatten()
@@ -588,7 +610,7 @@ class LoSweep:
         return Z
 
 
-    def run_sweep(self, chanmask_file: Path, tone_list: npt.NDArray, N_steps=500, freq_step=1.0, pd: QThreadJobProgressDialog | None=None) -> Future[LoSweepData]:
+    def run_sweep(self, chanmask_file: Path, tone_list: npt.NDArray, N_steps=500, freq_step=1e3, pd: QThreadJobProgressDialog | None=None) -> Future[LoSweepData]:
         """Perform a stepped frequency sweep centered at f_center and save result as s21.npy file
 
         f_center: center frequency for sweep in [MHz], default is 400
@@ -599,20 +621,20 @@ class LoSweep:
         #    print(freqs)
         log = logging.getLogger()
         if len(self.freqs) > 1:
-            tone_diff = np.diff(self.freqs)[0] / 1e6  # MHz
+            tone_diff = np.diff(self.freqs)[0] * 1e-6  # MHz
         else:
             tone_diff = 0
         log.info(f"tone diff={tone_diff}")
         if freq_step > 0:
-            flo_step = freq_step
+            flo_step = freq_step * 1e-6  # MHz
         else:
             flo_step = tone_diff / N_steps
 
         log.info(f"lo step size={flo_step}")
-        flo_start = self.f_center - flo_step * N_steps / 2.0  # 256
-        flo_stop = self.f_center + flo_step * N_steps / 2.0  # 256
+        flo_start = self.f_center * 1e-6 - flo_step * N_steps / 2.0  # MHz
+        flo_stop = self.f_center * 1e-6  + flo_step * N_steps / 2.0  # MHz
 
-        self.flos = np.arange(flo_start, flo_stop, flo_step) #+1e-6
+        self.flos = np.arange(flo_start, flo_stop, flo_step)  # MHz
         if pd is not None:
             pd.setMaximum(len(self.flos))
             pd.setLabelText('Performing LO Sweep...')
@@ -644,7 +666,7 @@ class LoSweep:
         f = np.zeros([np.size(self.freqs), np.size(self.flos)])
         log.info(f"shape of f = {f.shape}")
         for itone, ftone in enumerate(self.freqs):
-            f[itone, :] = self.flos * 1.0e6 + ftone
+            f[itone, :] = self.flos * 1e6 + ftone
         #    f = np.array([flos * 1e6 + ftone for ftone in freqs]).flatten()
         sweep_Z_f = sweep_Z.T
         #    sweep_Z_f = sweep_Z.T.flatten()
@@ -654,12 +676,12 @@ class LoSweep:
         # WITH TIMESTAMP
 
         # set the LO back to the original frequency
-        self.valon.set_frequency(valon5009.SYNTH_B, self.f_center)
+        self.valon.set_frequency(valon5009.SYNTH_B, self.f_center * 1e-6)
 
         # return (f, sweep_Z_f)
         # TODO: Fix this
         # chanmask = get_chanmask(chanmask_file)
         chanmask = np.ones_like(self.tone_list)
-        self.data = LoSweepData(self.tone_list, np.array((f, sweep_Z_f)), chanmask)
+        self.data = LoSweepData(self.tone_list, self.f_center, np.array((f, sweep_Z_f)), chanmask)
         self._processed = True
         print("LO Sweep s21 file saved.")

rfsocinterface/gui/lodiagnostics.py
@@ -20,6 +20,7 @@ from PySide6.QtWidgets import (
     QWidget,
     QDialog,
     QMessageBox,
+    QLayout
 )
 
 from rfsocinterface.core.losweep import LoSweepData, ResonatorData, get_tone_list
@@ -30,7 +31,7 @@ from rfsocinterface.core.utils import PathLike
 
 DPI = 100
 
-EPSILON = 1e-4  # Max x difference to count as the mouse being close to the line
+EPSILON = 1e3  # Max x difference in Hz to count as the mouse being close to the line
 
 
 class ResonatorDialog(QDialog, Ui_ResonatorDialog):
@@ -52,6 +53,7 @@ class ResonatorDialog(QDialog, Ui_ResonatorDialog):
         """Initialize a ResonatorWindow."""
         super().__init__(parent=parent)
         self.setupUi(self)
+        self.layout().setSizeConstraint(QLayout.SizeConstraint.SetNoConstraint)
         self.resonator = resonator
         self.dragging = False
         self.error_label = None
@@ -63,7 +65,7 @@ class ResonatorDialog(QDialog, Ui_ResonatorDialog):
         self.set_figure(fig)
         self.canvas.line.set_label('New Frequency')
         self.ax.axvline(
-            self.resonator.tone * 1e-6,
+            self.resonator.tone,
             0,
             1,
             color='gray',
@@ -73,7 +75,7 @@ class ResonatorDialog(QDialog, Ui_ResonatorDialog):
         self.ax.legend()
 
         # Fill in the necessary values in the UI
-        self.old_freq_value_label.setText(f'{self.resonator.tone * 1e-6:.3f}')
+        self.old_freq_value_label.setText(f'{self.resonator.tone * 1e-6:.5f}')
         self.depth_value_label.setText('N/A')  # TODO: Resonance depth
 
         # Temporary values for saving / undoing changes
@@ -84,7 +86,7 @@ class ResonatorDialog(QDialog, Ui_ResonatorDialog):
         # Setup text validator
         freq_range = self.ax.get_xlim()
         self.validator = QDoubleValidator(
-            freq_range[0], freq_range[1] + 0.001, 5, parent=self
+            freq_range[0] * 1e-6, freq_range[1] * 1e-6 + 0.001, 9, parent=self
         )
         self.new_freq_lineEdit.setValidator(self.validator)
 
@@ -98,10 +100,10 @@ class ResonatorDialog(QDialog, Ui_ResonatorDialog):
             self.reject_changes
         )
         self.refit_pushButton.clicked.connect(self.refit)
-        self.new_freq_lineEdit.textChanged.connect(self.change_freq)
+        self.new_freq_lineEdit.textEdited.connect(self.change_freq)
 
         # This line will call change_freq since the signal has been connected
-        self.new_freq_lineEdit.setText(f'{self.resonator.fit_f0 * 1e-6:.3f}')
+        self.new_freq_lineEdit.setText(f'{self.resonator.fit_f0 * 1e-6:.9f}')
 
     def accept_changes(self):
         """Handle accepting changes."""
@@ -113,6 +115,24 @@ class ResonatorDialog(QDialog, Ui_ResonatorDialog):
     def reject_changes(self):
         """Handle rejecting changes."""
         self.reject()
+    
+    def move_line(self, x: float):
+        """Move the line to the specified x value."""
+        self.canvas.line.set_xdata([x, x])
+        self.temp_fit_f0 = x
+        self.new_freq_lineEdit.setText(f'{x * 1e-6:.9f}')
+        self.delta_value_label.setText(
+            f'{(x - self.resonator.tone) * 1e-3:.3f}'
+        )
+        self.figcanvas.draw_idle()
+
+        new_freq = float(new_freq) * 1e6  # Convert MHz to Hz
+        self.temp_fit_f0 = new_freq 
+        self.canvas.line.set_xdata([new_freq, new_freq])
+        self.figcanvas.draw_idle()
+        self.delta_value_label.setText(
+            f'{(new_freq - self.resonator.tone) * 1e-3:.3f}'
+        )
 
     def refit(self):
         """Refit the resonator."""
@@ -122,16 +142,18 @@ class ResonatorDialog(QDialog, Ui_ResonatorDialog):
         self.temp_fit_f0 = fit_f0
         self.temp_fit_qc = fit_qc
         self.temp_fit_qi = fit_qi
-        self.new_freq_lineEdit.setText(f'{np.real(fit_f0) * 1e-6:.3f}')
+        self.new_freq_lineEdit.setText(f'{np.real(fit_f0 * 1e-6):.9f}')
+        self.canvas.line.set_xdata([fit_f0, fit_f0])
+        self.figcanvas.draw_idle()
 
     def reset_freq(self):
         """Reset the line to the initial frequency."""
-        self.new_freq_lineEdit.setText(f'{self.resonator.fit_f0 * 1e-6:.3f}')
+        self.new_freq_lineEdit.setText(f'{self.resonator.fit_f0 * 1e-6:.9f}')
 
     def change_freq(self):
         """Handle changes to the frequency in the lineEdit."""
         freq_range = self.ax.get_xlim()
-        new_freq = self.new_freq_lineEdit.text()
+        new_freq = self.new_freq_lineEdit.text() 
 
         valid = self.validator.validate(new_freq, 0)[0]
 
@@ -145,7 +167,7 @@ class ResonatorDialog(QDialog, Ui_ResonatorDialog):
             if self.error_label is None:
                 self.error_label = QLabel(self)
                 self.error_label.setText(
-                    f'New frequency must be in the range [{freq_range[0]:.3f}, {freq_range[1]:.3f}]'
+                    f'New frequency must be in the range [{freq_range[0] * 1e-6:.3f}, {freq_range[1] * 1e-6:.3f}] MHz'
                 )
                 self.error_label.setStyleSheet('color: red;')
                 self.formLayout.insertRow(2, None, self.error_label)
@@ -158,12 +180,12 @@ class ResonatorDialog(QDialog, Ui_ResonatorDialog):
             self.new_freq_lineEdit.setStyleSheet('')
 
             # Update the line's position
-            new_freq = float(new_freq)
-            self.temp_fit_f0 = new_freq * 1e6
+            new_freq = float(new_freq) * 1e6  # Convert MHz to Hz
+            self.temp_fit_f0 = new_freq 
             self.canvas.line.set_xdata([new_freq, new_freq])
             self.figcanvas.draw_idle()
             self.delta_value_label.setText(
-                f'{(new_freq - self.resonator.tone * 1e-6) * 1e3:.3f}'
+                f'{(new_freq - self.resonator.tone) * 1e-3:.3f}'
             )
 
     def set_figure(self, fig: Figure):
@@ -193,9 +215,9 @@ class ResonatorDialog(QDialog, Ui_ResonatorDialog):
             # Stop dragging and update the line's position
             self.dragging = False
             self.setCursor(Qt.CursorShape.OpenHandCursor)
-            self.new_freq_lineEdit.setText(f'{event.xdata:.5f}')
+            self.new_freq_lineEdit.setText(f'{event.xdata * 1e-6:.9f}')
             self.canvas.line.set_xdata([event.xdata, event.xdata])
-            self.temp_fit_f0 = event.xdata * 1e6
+            self.temp_fit_f0 = event.xdata
             self.figcanvas.draw_idle()
 
     def mouse_press(self, event: MouseEvent):
@@ -233,9 +255,9 @@ class ResonatorDialog(QDialog, Ui_ResonatorDialog):
             return  # Not left clicking
 
         # Moving while holding left mouse and dragging, so update the line's position
-        self.new_freq_lineEdit.setText(f'{event.xdata:.5f}')
+        self.new_freq_lineEdit.setText(f'{event.xdata * 1e-6:.9f}')
         self.canvas.line.set_xdata([event.xdata, event.xdata])
-        self.temp_fit_f0 = event.xdata * 1e6
+        self.temp_fit_f0 = event.xdata 
         self.figcanvas.draw_idle()
 
 