6088ec0e9e286bd6307a4582ef093f6445b91af4
Nia McNichols <niamcnichols@gmail.com>
2025-05-20 17:00:05 -0700

    Add Beginnings of Logging

--- rfsocinterface/__init__.py
+++ rfsocinterface/__init__.py
@@ -1,3 +1,6 @@
 """A user-friendly GUI for configuring and monitoring MKID readout software."""
 
 __version__ = '0.1.0'
+
+import logging.config
+logging.config.fileConfig('rfsocinterface/logging.conf')
