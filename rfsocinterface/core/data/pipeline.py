7ac18bf825093caa183a1d95366f9d4739c7b929
jacksayers <jack@its.caltech.edu>
2025-11-25 16:11:17 -0800

    Test Zenith Dithers

--- rfsocinterface/core/data/pipeline.py
+++ rfsocinterface/core/data/pipeline.py
@@ -186,7 +186,7 @@ class DataPipeline:
         _logger.info(f'Data pipeline completed in {stop_time - start_time:.3f} seconds.')
         return output
 
-def find_peaks(data: ProcessedData):
+def find_peaks(data: ProcessedData, primary_direction: str='az'):
     import numpy as np
     from numpy.polynomial import Polynomial
     # find peak going forward / back
@@ -197,6 +197,7 @@ def find_peaks(data: ProcessedData):
     i_res = 241
     right_indices = np.argwhere(np.logical_and(10 <= data.time, data.time <= 15)).flatten()
     left_indices = np.argwhere(np.logical_and(20 <= data.time, data.time <= 25)).flatten()
+    telescope_pos = data.detector_az[i_res] if primary_direction.lower() == 'az' else data.detector_za[i_res]
 
     right_peak_idx = right_indices[np.argmax(data.data_mK[i_res, right_indices])]
     left_peak_idx = left_indices[np.argmax(data.data_mK[i_res, left_indices])]
@@ -204,14 +205,15 @@ def find_peaks(data: ProcessedData):
     right_slice = slice(right_peak_idx - 2, right_peak_idx + 3)
     left_slice = slice(left_peak_idx - 2, left_peak_idx + 3)
 
-    right_fit = Polynomial.fit(data.detector_az[i_res, right_slice], data.data_mK[i_res, right_slice], 2).convert()
-    left_fit = Polynomial.fit(data.detector_az[i_res, left_slice], data.data_mK[i_res, left_slice], 2).convert()
+    right_fit = Polynomial.fit(telescope_pos[right_slice], data.data_mK[i_res, right_slice], 2).convert()
+    left_fit = Polynomial.fit(telescope_pos[left_slice], data.data_mK[i_res, left_slice], 2).convert()
 
     right_az_0 = (-1 * right_fit.coef[1]) / (2 * right_fit.coef[2])
     left_az_0 = (-1 * left_fit.coef[1]) / (2 * left_fit.coef[2])
-    plt.plot(data.detector_az[i_res, right_slice], data.data_mK[i_res, right_slice], label=f'Right AZ_0 = {right_az_0}')
-    plt.plot(data.detector_az[i_res, left_slice], data.data_mK[i_res, left_slice], label=f'Left AZ_0 = {left_az_0}')
-    scan_rate = (data.detector_az[241, right_peak_idx + 10] - data.detector_az[241, right_peak_idx - 10]) \
+    plt.plot(telescope_pos[:], data.data_mK[i_res, :], label=f'Full Trace')
+    plt.plot(telescope_pos[right_slice], data.data_mK[i_res, right_slice], label=f'Right {primary_direction.upper()}_0 = {right_az_0}')
+    plt.plot(telescope_pos[left_slice], data.data_mK[i_res, left_slice], label=f'Left {primary_direction.upper()}_0 = {left_az_0}')
+    scan_rate = (telescope_pos[right_peak_idx + 10] - telescope_pos[right_peak_idx - 10]) \
         / (data.time[right_peak_idx + 10] - data.time[right_peak_idx - 10])
     time_delay = (left_az_0 - right_az_0) / scan_rate / 2  # Amount RFSoC is behind the telescope
     plt.annotate(f'Time Delay (seconds RFSoC lags behind telescope)= {time_delay:.3f}s', (.1, .1), xycoords='axes fraction')
@@ -227,12 +229,13 @@ if __name__ == '__main__':
     # setnum = 1017
 
     #Telescope Testing
-    date = '20251118'
-    setnum = 1016
+    date = '20251125'
+    setnum = 1025
 
     dataset = 'data_mK'
     beam_map_mode = False 
     do_electronics_noise_removal = True
+    primary_direction = 'az'
 
     ds_factor = 8
     hp_filt_freq = 0.05
@@ -256,8 +259,8 @@ if __name__ == '__main__':
     pipeline.add_routine(hpfilt)
     pipeline.add_routine(lpfilt)
     # pipeline.add_routine(psd)
-    # pipeline.add_routine(cleaner)
-    # pipeline.add_routine(binner)
+    pipeline.add_routine(cleaner)
+    pipeline.add_routine(binner)
 
     data = pipeline.run_pipeline(date, setnum)
 #     from rfsocinterface.analysis.psd import plot_psd
@@ -265,6 +268,7 @@ if __name__ == '__main__':
 #     psd = data.get_node_value('psd_gain_phase')[:]
 #     plot_psd(freq, psd, 'test.pdf', basis='gp')
 #     plt.show()
-    find_peaks(data)
-    pdb.set_trace()
+    find_peaks(data, primary_direction=primary_direction)
+    data.plot()
+    # pdb.set_trace()
     data.close()
