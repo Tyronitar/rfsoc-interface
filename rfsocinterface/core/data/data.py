Fix Issues with Downsampling and Basis Rotation

rfsocinterface/core/data/data.py
@@ -275,8 +275,8 @@ def rotate_basis(
     out_data[0] = np.cos(rotation_angle)[:, np.newaxis] * in_data[0] - \
         np.sin(rotation_angle)[:, np.newaxis] * in_data[1]
 
-    out_data[1] = np.sin(rotation_angle)[:, np.newaxis] * in_data[0] - \
-        np.sin(rotation_angle)[:, np.newaxis] * in_data[1]
+    out_data[1] = np.sin(rotation_angle)[:, np.newaxis] * in_data[0] + \
+        np.cos(rotation_angle)[:, np.newaxis] * in_data[1]
 
 
 # TODO: Optimize this for chunked data
@@ -1096,7 +1096,8 @@ class ConsolidatedData(NewDataStorage):
             use_filter=False,
         )
         raw_data.close()
-        global_data_group.attrs['fs'] = 1 / (timestamp[1] - timestamp[0])
+        fs = 1 / (timestamp[1] - timestamp[0])
+        global_data_group.attrs['fs'] = fs
 
 
         # Intiialize group for storing data per-channel
@@ -1129,7 +1130,7 @@ class ConsolidatedData(NewDataStorage):
             tones_table['delta_y'] = raw_data.detector_delta_y[:]
             tones_table['beam_amplitude'] = raw_data.detector_beam_ampl[:]
             tones_table['polarization']  = raw_data.detector_pol[:]
-            tones_table['dfoverf_per_mK'] = raw_data. dfoverf_per_mK[:]
+            tones_table['dfoverf_per_mK'] = raw_data.dfoverf_per_mK[:] * -1
             chanmask = raw_data.chanmask[:]
             off_res = np.argwhere(chanmask == 0).flatten()
             no_pol = np.argwhere(tones_table['polarization'] < 1).flatten()
@@ -1301,17 +1302,6 @@ class ConsolidatedData(NewDataStorage):
             del time_ordered_data_group['temp_detector_az']
             del time_ordered_data_group['temp_detector_za']
             
-        # iq = cd.get('/channels/channel_000/time_ordered_data/temp_data_IQ')
-        # plt.plot(temp_timestamp[:], temp_data_IQ[0, 0], label='Full data')
-        # plt.plot(timestamp[:], data_IQ[0, 0], label='Downsampled data')
-        # plt.legend()
-        # plt.show()
-        # plt.scatter(temp_detector_az[0], temp_detector_za[0], label='Full data')
-        # plt.scatter(detector_az[0], detector_za[0], label='Downsampled data')
-        # plt.legend()
-        # plt.show()
-        # pdb.set_trace()
-
         # Get rid of full timestamp now
         del global_data_group['temp_timestamp']
 
@@ -1421,16 +1411,15 @@ class ProcessedData(NewDataStorage):
             calibration_info['IQ_to_freq_diss_angle'] = IQ_to_freq_diss_angle
             calibration_info['adc_units_to_hz'] = adc_units_to_hz 
 
+            detector_f = tones_table['baseband_freq'] + channel_group.attrs['lo_freq']
             df_per_mK = compute_df_per_mK(
                 tones_table['polarization'],
                 tones_table['beam_amplitude'],
-                tones_table['baseband_freq'],
+                detector_f,
                 tones_table['dfoverf_per_mK'],
             )
             calibration_info['df_per_mK'] = df_per_mK
 
-            # First mean center IQ data
-            data_IQ[:] = data_IQ[:] - np.mean(data_IQ, axis=-1, keepdims=True)
 
             # Rotate to Gain / Phase
             IQ_to_gain_phase_angle = np.atan2(carrier_amplitudes[0], carrier_amplitudes[1])
@@ -1441,6 +1430,8 @@ class ProcessedData(NewDataStorage):
                 IQ_to_gain_phase_angle,
             )
             # Generate calibrated data
+            # First mean center IQ data
+            data_IQ[:] = data_IQ[:] - np.mean(data_IQ, axis=-1, keepdims=True)
             generate_calibrated_data(
                 data_IQ,
                 data_freq_diss,
@@ -1449,6 +1440,23 @@ class ProcessedData(NewDataStorage):
                 adc_units_to_hz,
                 df_per_mK,
             )
+
+            # fig, axes = plt.subplots(2, 3)
+            # fig.suptitle('Datasets before further processing')
+            # axes[0, 0].set_title('data_I')
+            # axes[0, 0].plot(data_IQ[0, 241])
+            # axes[1, 0].set_title('data_Q')
+            # axes[1, 0].plot(data_IQ[1, 241])
+            # axes[0, 1].set_title('data_gain')
+            # axes[0, 1].plot(data_gain_phase[0, 241])
+            # axes[1, 1].set_title('data_phase')
+            # axes[1, 1].plot(data_gain_phase[1, 241])
+            # axes[0, 2].set_title('data_freq')
+            # axes[0, 2].plot(data_freq_diss[0, 241])
+            # axes[1, 2].set_title('data_diss')
+            # axes[1, 2].plot(data_freq_diss[1, 241])
+            # plt.show()
+            # pdb.set_trace()
 
         # Make virtual datasets for the new stuff
         total_tones = self['vdsets'].attrs['n_tones']

rfsocinterface/core/data/routines.py
@@ -366,7 +366,8 @@ class RemoveElectronicsNoise(DataRoutine):
                     BUTTER_ORDER,
                     lp_filt_freq,
                     btype='low',
-                    fs=fs,output='sos',
+                    fs=fs,
+                    output='sos',
                     analog=False,
                 )
                 clean_gain_phase = signal.sosfiltfilt(filt_sos, clean_gain_phase)
@@ -382,12 +383,6 @@ class RemoveElectronicsNoise(DataRoutine):
                 numerator = np.einsum('ijk,ik->ij', clean_gain_phase[:, subtraction_indices], templates[:, i_mode])  # 2 x N_tones
                 corr = numerator / denominator[:, i_mode:i_mode+1]  # 2 x N_tones
                 clean_gain_phase[:, subtraction_indices] = clean_gain_phase[:, subtraction_indices] - np.einsum('ij,ikl->ijl', corr, templates[:, i_mode:i_mode+1])  # 2 x N_tones x N_samples
-            # fig, axes = plt.subplots(2, 2)
-            # for row in range(2):
-            #     for col in range(2):
-            #         axes[row, col].plot(templates[row, col])
-            # plt.show()
-            # pdb.set_trace()
             
             # Apply clean data
             data_gain_phase[:] = clean_gain_phase
@@ -411,17 +406,22 @@ class RemoveElectronicsNoise(DataRoutine):
                 calibration_info['adc_units_to_hz'],
                 calibration_info['df_per_mK'],
             )
-            # fig, axes = plt.subplots(1, 3)
-            # axes[0].plot(data_IQ[0, 0])
-            # axes[0].set_title('data_I')
-            # axes[1].plot(data_gain_phase[0, 0])
-            # axes[1].set_title('data_gain')
-            # axes[2].plot(data_mK[0])
-            # axes[2].set_title('data_mK')
+            # fig, axes = plt.subplots(2, 3)
+            # fig.suptitle('Datasets after noise removal')
+            # axes[0, 0].set_title('data_I')
+            # axes[0, 0].plot(data_IQ[0, 241])
+            # axes[1, 0].set_title('data_Q')
+            # axes[1, 0].plot(data_IQ[1, 241])
+            # axes[0, 1].set_title('data_gain')
+            # axes[0, 1].plot(data_gain_phase[0, 241])
+            # axes[1, 1].set_title('data_phase')
+            # axes[1, 1].plot(data_gain_phase[1, 241])
+            # axes[0, 2].set_title('data_freq')
+            # axes[0, 2].plot(data_freq_diss[0, 241])
+            # axes[1, 2].set_title('data_diss')
+            # axes[1, 2].plot(data_freq_diss[1, 241])
             # plt.show()
             # pdb.set_trace()
-
-
 
         self.params['eigenmodes'] = eigenmodes
         return inputs

rfsocinterface/core/utils.py
@@ -34,7 +34,7 @@ import h5py
 import numpy as np
 import numpy.typing as npt
 from scipy import ndimage
-from scipy.signal import sosfilt, sosfilt_zi, cheby1
+from scipy.signal import sosfilt, sosfilt_zi, cheby1, group_delay, sos2tf
 from scipy.signal import resample_poly
 import redis
 
@@ -541,9 +541,12 @@ def chunked_downsample(
             dec = resample_poly(chunk, up=1, down=q, axis=axis)
         else:
             read_start = max(0, start)
+            # read_start = int(q * np.ceil(start / q))  # Start reading at multiple of q
             read_stop = min(N, start + chunk_size)
             chunk = axis_slice(dset, start=read_start, stop=read_stop, axis=axis)
-            dec = axis_slice(chunk, step=q, axis=axis)
+            # dec = axis_slice(chunk, step=q, axis=axis)
+            dec_start = int(q * np.ceil(start / q)) - start  # Make sure we start on a multiple of q
+            dec = axis_slice(chunk, start=dec_start, step=q, axis=axis)
 
         valid_start = downsampled_equiv(start - read_start)
         valid_stop = valid_start + min(downsampled_equiv(min(chunk_size, N - start)), out_dset.shape[axis] - out_index)
@@ -946,9 +949,19 @@ def decimate_in_chunks(x: npt.NDArray, q: int, axis: int = -1, padlen: int | Non
     else:
         out[...] = axis_slice(y, step=q, axis=axis)
 
-def new_decimate_in_chunks(dset: h5py.Dataset, out_dset, q, axis=-1, chunk_shape=None):
+def new_decimate_in_chunks(dset: h5py.Dataset, out_dset, q: int, axis=-1, chunk_shape=None):
 
-    sos = cheby1(8, 0.05, 0.8 / q, output="sos")
+    # Copmpute values to account for phase lag from the filter
+    wc = 0.8 / q
+    sos = cheby1(8, 0.05, wc, output="sos")
+    b, a = sos2tf(sos)
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', r'^The filter\'s denominator')
+        w, gd = group_delay((b, a))
+    passband = w <= wc * np.pi
+    gd_passband = gd[passband]
+    delay = int(np.ceil(np.median(gd_passband)))
+    delay_out = int(np.round(delay / q))
 
     n_sections = sos.shape[0]
 
@@ -960,8 +973,6 @@ def new_decimate_in_chunks(dset: h5py.Dataset, out_dset, q, axis=-1, chunk_shape
     zi = zi.reshape((n_sections, *zi_shape))
     x0 = axis_slice(dset, stop=1, axis=axis)
     zi = x0 * zi
-
-
     N = dset.shape[axis]
 
     out_pos = 0
@@ -970,6 +981,8 @@ def new_decimate_in_chunks(dset: h5py.Dataset, out_dset, q, axis=-1, chunk_shape
     chunk = np.empty(chunk_shape, dtype=dset.dtype)
     chunk_size = chunk_shape[axis]
     y = np.empty_like(chunk)
+
+    out_pos -= delay_out
 
     for start in range(0, N, chunk_size):
 
@@ -980,36 +993,37 @@ def new_decimate_in_chunks(dset: h5py.Dataset, out_dset, q, axis=-1, chunk_shape
             chunk = np.empty(shape, dtype=dset.dtype)
             y = np.empty_like(chunk)
 
-        # sl = get_axis_slice(dset, start, stop, axis=axis)
-        # dset.read_direct(chunk, source_sel=sl)
         chunk[:] = axis_slice(dset, start, stop, axis=axis)
-        # chunk = axis_slice(dset, start, stop, axis=axis, direct_read=True)
-        # chunk = dset[sl]
 
-        # filter chunk
-        # pdb.set_trace()
-        # x0 = axis_slice(chunk, stop=1, axis=axis)
-        # zf = x0 * zi
         with warnings.catch_warnings():
             warnings.filterwarnings('ignore', r'^Invalid value encountered in')
             y[:], zi = sosfilt(sos, chunk, axis=axis, zi=zi)
 
         # decimate
-        dec = axis_slice(y, step=q, axis=axis)
+        dec_start = int(q * np.ceil(start / q)) - start  # Make sure we start on a multiple of q
+        dec = axis_slice(y, start=dec_start, step=q, axis=axis)
 
         n_out = dec.shape[axis]
 
         write_start = out_pos
+        if write_start < 0:
+            # Skip the fisrt `delay_out` samples
+            dec = axis_slice(dec, start=delay_out, axis=axis)
+            write_start = 0
+            out_pos = 0
+            n_out = dec.shape[axis]
         write_stop = min(out_pos + n_out, out_dset.shape[axis])
 
         out_sl = get_axis_slice(out_dset, start=write_start, stop=write_stop, axis=axis)
 
-        # out_sl = [slice(None)] * out_dset.ndim
-        # out_sl[axis] = slice(out_pos, min(out_pos + n_out, out_dset.shape[axis]))
-
-        out_dset[tuple(out_sl)] = axis_slice(dec, stop=write_stop-write_start, axis=axis)
+        out_dset[out_sl] = axis_slice(dec, stop=write_stop-write_start, axis=axis)
 
         out_pos += n_out
+
+    # Use reflect padding for the last `delay_out` samples
+    same_slice = get_axis_slice(out_dset, start=out_pos, axis=axis)
+    last_values = np.take(out_dset, np.arange(out_pos - 1, out_pos - delay_out - 1, -1), axis=axis)
+    out_dset[same_slice] = last_values
 
 
 def iterate_chunks(x: npt.NDArray | h5py.Dataset, chunk_size: int=None, axis: int=-1) -> Iterator[tuple[int, int, npt.NDArray]]: