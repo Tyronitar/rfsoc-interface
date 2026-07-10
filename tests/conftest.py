0603b906459fefc5f421b4ccbdff6b6213f9692f
Nia McNichols <niamcnichols@gmail.com>
2026-07-09 17:50:50 -0700

    Add Data Files to Tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,8 @@
 """Shared testing utilities."""
+
+from pathlib import Path
+import pytest
+
+FIXTURE_DIRECTORY = Path(__file__).parent.resolve() / 'test_data'
+
+TOD_FILE = pytest.mark.datafiles(FIXTURE_DIRECTORY / '20260709_Test_Tile_TOD_set1001.h5')
